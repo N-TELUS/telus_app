ba3a1f8593983781152474f991f833161599a5df
Ryota-nino <ryota.swimmer@icloud.com>
2022-10-30 00:50:25 +0900

    `add` テーマカラー定義

---
+++ lib/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const cardColor = const Color(0xFF222831);
+  static const primaryColor = const Color(0xFF00ADB5);
+  static const fontColor = const Color(0xFFEEEEEE);
+  static const bgColor = const Color(0xFF393E46);
+}
