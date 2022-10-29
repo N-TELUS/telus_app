89e1e2abe353fa1fbef6ffe75d42b037eb7d4fea
Ryota-nino <ryota.swimmer@icloud.com>
2022-10-30 00:51:53 +0900

    `add` スケジュール画面仮配置

---
+++ lib/screens/schedule/schedule_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ScheduleScreen extends StatefulWidget {
+  ScheduleScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ScheduleScreen> createState() => _ScheduleScreenState();
+}
+
+class _ScheduleScreenState extends State<ScheduleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("SCHEDULE SCREEN"),
+      ),
+    );
+  }
+}
